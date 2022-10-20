ee9d103a84da496710483dea9f66bb122653ace0
Elyssa McMaster <elyssamcmaster@gmail.com>
2022-10-20 19:54:27 +0200

    Update clusterby2.py

--- clusterby2.py
+++ clusterby2.py
@@ -1,3 +1,14 @@
+import tensorflow as tf 
+import os
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+
+
+
+'''
 import numpy as np 
 import matplotlib.pyplot as plt 
 import os
@@ -14,3 +25,4 @@ for category in CATEGORIES:
         plt.show()
         break
     break
+'''
